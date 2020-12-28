82eef81597346f1d9c94ead07f7045933e6aacc5
Ehotuwe <76240894+Ehotuwe@users.noreply.github.com>
2020-12-28 22:17:30 +0300

    a2 complete

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -5,7 +5,7 @@ Queue priorities are from 0 to 10
 """
 from typing import Any
 
-
+d={}
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
     Operation that add element to the end of the queue
@@ -13,8 +13,14 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
-    return None
-
+    global d
+    v = [elem]
+    k = priority
+    if k in d:
+        d[k].append(elem)
+    else:
+        d.update({k: v})
+    d = {k: v for k, v in sorted (d.items (), key=lambda item: item[0])}
 
 def dequeue() -> Any:
     """
@@ -22,7 +28,9 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
-    return None
+    for item in d:
+        if len(d[item]):
+            return d[item].pop(0)
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -32,7 +40,9 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None
+    for item in d:
+        if 0 <= ind < len (d[item]):
+            return d[priority][ind]
 
 
 def clear() -> None:
@@ -41,4 +51,4 @@ def clear() -> None:
 
     :return: None
     """
-    return None
+    d.clear()
