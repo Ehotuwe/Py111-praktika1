412b94353c628212c883f5772ca16888105f724e
Ehotuwe <76240894+Ehotuwe@users.noreply.github.com>
2021-01-21 18:59:36 +0300

    e2 complete

--- Tasks/e2_dijkstra.py
+++ Tasks/e2_dijkstra.py
@@ -9,5 +9,21 @@ def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, U
     :param starting_node: starting node from g
     :return: dict like {'node1': 0, 'node2': 10, '3': 33, ...} with path costs, where nodes are nodes from g
     """
-    print(g, starting_node)
-    return dict()
+
+    l = list (g)
+    d = {k: float ('inf') for k in l}
+    d[starting_node] = 0
+    neighbors = []
+    queue = [starting_node]
+
+    while queue:
+        current_node = queue.pop ()
+        neighbors.append (current_node)
+        for node in g.neighbors (current_node):
+            if node not in neighbors and node not in queue:
+                queue.append (node)
+
+            if d[node] > g[current_node][node]['weight'] + d[current_node]:
+                d[node] = g[current_node][node]['weight'] + d[current_node]
+
+    return d
