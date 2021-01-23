502bfc6339ef6782b8312cf74ca9037d848c310a
Ehotuwe <76240894+Ehotuwe@users.noreply.github.com>
2021-01-23 12:32:09 +0300

    h0  complete new

--- Tasks/h0_KMP.py
+++ Tasks/h0_KMP.py
@@ -8,22 +8,24 @@ def _prefix_fun(prefix_str: str) -> List[int]:
     :param prefix_str: dubstring for prefix function
     :return: prefix values table
     """
-    list_p_f=[0]*len(prefix_str)
-    i=0
-    j=1
+    list_p_f = [0] * len(prefix_str)
+    i = 0
+    j = 1
 
-    while j<len(prefix_str):
-        if prefix_str[i]==prefix_str[j]:
-            list_p_f[j]=i+1
-            i+=1
-            j+=1
+    while j < len(prefix_str):
+        if prefix_str[i] == prefix_str[j]:
+            list_p_f[j] = i + 1
+            i += 1
+            j += 1
 
         else:
-            if i==0:
-                j+=1
+            if i == 0:
+                j += 1
             else:
-                i=list_p_f[i-1]
+                i = list_p_f[i - 1]
     return list_p_f
+
+
 def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
     """
     Implementation of Knuth-Morrison-Pratt algorithm
@@ -35,17 +37,17 @@ def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
 
     i = 0
     j = 0
-    pref=_prefix_fun(substr)
-    while j<len(inp_string):
-        if substr[i]==inp_string[j]:
-            i+=1
-            j+=1
-            if i==len(substr):
-                return j-len(substr)
+    pref = _prefix_fun(substr)
+    while j < len(inp_string):
+        if substr[i] == inp_string[j]:
+            i += 1
+            j += 1
+            if i == len(substr):
+                return j - len(substr)
         else:
-            if i==0:
-                j+=1
+            if i == 0:
+                j += 1
             else:
-                i=pref[i-1]
+                i = pref[i - 1]
 
     return None
