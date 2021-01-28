3eaf81d4385f28d746abb547dee9eeea6d488bd2
Ehotuwe <76240894+Ehotuwe@users.noreply.github.com>
2021-01-28 23:27:41 +0300

    g1 complete

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,26 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    count = len (container)
+    if count > 2:
+        part_1 = sort (container[:count // 2])
+        part_2 = sort (container[count // 2:])
+        container = part_1 + part_2
+        last_index = len (container) - 1
+
+        for i in range (last_index):
+            min_value = container[i]
+            min_index = i
+
+            for j in range (i + 1, last_index + 1):
+                if min_value > container[j]:
+                    min_value = container[j]
+                    min_index = j
+
+            if min_index != i:
+                container[i], container[min_index] = container[min_index], container[i]
+
+    elif len (container) > 1 and container[0] > container[1]:
+        container[0], container[1] = container[1], container[0]
+
     return container
